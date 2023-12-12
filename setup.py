27567708e75f8203cfe240c5907ab13f09ece418
Plinio Zanini <pliniozanini@hotmail.com>
2023-12-12 14:30:25 -0300

    fix: package name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my-financial-package',
+    name='portfolio-management-tools',
     version='1.0',
     packages=find_packages(),
     install_requires=[
